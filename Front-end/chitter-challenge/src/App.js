db8fedbb10710e77933f25cdc14d7f22d00dfb2e
Jack Dowdeswell <jack.dowdeswell@outlook.com>
2021-12-18 15:39:37 +0000

    Basic front-end peep form

---
+++ Front-end/chitter-challenge/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import PeepForm from './Components/PeepForm';
+
+function App() {
+  return (
+    <>
+      <body className="app">
+        <h1 className="chitterTitle">Chitter </h1><h1 className="bird">🐤</h1>
+        <PeepForm />
+      </body>
+    </>
+  );
+}
+
+export default App;
---
+++ Front-end/chitter-challenge/src/Components/PeepForm.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function PeepForm() {
+    return (
+        <div class="mb-3 container">
+            <label for="peepTextbox" class="form-label">Start typing your peep in the box below</label>
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+        </div>
+    )
+}
