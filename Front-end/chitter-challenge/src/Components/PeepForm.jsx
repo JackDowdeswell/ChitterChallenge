1f0423b122844d4ea06b50fe4c82b9ac8c4ffb2a
Jack Dowdeswell <jack.dowdeswell@outlook.com>
2021-12-18 16:05:56 +0000

    added a button to the form

--- Front-end/chitter-challenge/src/Components/PeepForm.jsx
+++ Front-end/chitter-challenge/src/Components/PeepForm.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 
 export default function PeepForm() {
     return (
-        <div class="mb-3 container">
-            <label for="peepTextbox" class="form-label">Start typing your peep in the box below</label>
-            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-        </div>
+        <>
+            <div class="mb-3 container">
+                <label for="peepTextbox" class="form-label">Start typing your peep in the box below</label>
+                <textarea class="form-control" id="peepTextbox" rows="3"></textarea>
+            </div>
+            <div class="mb-3 container">
+                <button type="button" class="btn btn-warning">Post Peep</button>
+            </div>
+        </>
     )
 }
