260e991c70d121b9c9947a69e09ad111a5f91e6a
Jack Dowdeswell <jack.dowdeswell@outlook.com>
2021-12-18 16:36:36 +0000

    Back-end folder created with server

---
+++ Back-end/server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import mongoose from 'mongoose';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded());
+
+// app.use(`/login`, login);
+// app.use(`/register`, register);
+
+
+// const main = async () => {
+//     console.log(`Connecting to DB @ mongodb://localhost:27017/chitter`);
+//     await mongoose.connect(`mongodb://localhost:27017/chitter`);
+// };
+
+// main().then(() => console.log(`Connected to DB`)).catch(err => console.log(err));
+
+app.listen(4000, () => console.log("Server started"));
